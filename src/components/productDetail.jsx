1e89f9b6cd94528d8d17336ab36627b2066c39cb
Fatima Deshmukh <96168561+Fatima2400@users.noreply.github.com>
2022-05-19 23:56:42 +0530

    Update productDetail.jsx

--- src/components/productDetail.jsx
+++ src/components/productDetail.jsx
@@ -14,6 +14,7 @@ export const Details =()=>{
     const Cart = useSelector((state) => state.Cart);
     const product = useSelector((state) => state.product);
     console.log(product)
+     const[cartBtn,setcartBtn]=useState("Add To Cart")
     const {id} = useParams();
   
   const dispatch = useDispatch();
@@ -43,21 +44,43 @@ axios.get(`https://bohemian1.herokuapp.com/arrivals/${id}`).then((res) =>{
       .then((res) => dispatch(getCartData(res.data)))
       .catch((err) => console.log(err));
   }
-  const handlecart=()=>{
+//   const handlecart=()=>{
     
-    axios.post('https://bohemian1.herokuapp.com/cartdata',{
-    title:product.title,
-    img:product.img,
-    price:product.price,
-    id:product.id,
-    qty:1
-})
-navigate('/cart')
-  //  window.location.href="/cart"
+//     axios.post('https://bohemian1.herokuapp.com/cartdata',{
+//     title:product.title,
+//     img:product.img,
+//     price:product.price,
+//     id:product.id,
+//     qty:1
+// })
+// navigate('/cart')
+//   //  window.location.href="/cart"
    
 
 
-}
+// }
+  
+  
+  
+  const handlecart=()=>{
+    if(cartBtn === 'Add To Cart'){
+        axios.post('https://bohemian1.herokuapp.com/cartdata',{
+            title:product.title,
+            img:product.img,
+            price:product.price,
+            id:product.id,
+            qty:1
+        })
+        cartPost()
+
+        setcartBtn("Remove from cart")
+    }else{
+        axios.delete(`https://bohemian1.herokuapp.com/cartdata/${id}`)
+       
+           setcartBtn("Add To Cart")
+       }
+  
+  }
  console.log(product)
 return (
   
@@ -77,7 +100,7 @@ return (
 <h5>₹{product.price}.00</h5>
 <p className='prodP'>{product.description}</p> 
 
-<button onClick={()=>{handlecart()}} className="cartAdd">ADD TO CART</button>
+<button onClick={()=>{handlecart()}} className="cartAdd">{cartBtn}</button>
 </div>
 
    </div> 
@@ -87,5 +110,105 @@ return (
    
 )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { getProductDetails } from "../redux/action";
+// import axios from 'axios';
+// import React, { useEffect, useState} from 'react'
+// import { useParams } from 'react-router-dom'
+// import { getCartData } from "../redux/action";
+
+
+// export const Details =()=>{
+//     const navigate = useNavigate();
+//     const Cart = useSelector((state) => state.Cart);
+//     const product = useSelector((state) => state.product);
+//     console.log(product)
+//     const {id} = useParams();
+  
+//   const dispatch = useDispatch();
+  
+//   useEffect(() => {
+//     getData()
+//     cartPost()
+//   },[])
+
+//   const getData=()=>{
+// axios.get(`https://bohemian1.herokuapp.com/arrivals/${id}`).then((res) =>{
+//     dispatch(getProductDetails(res.data))
+    
+//     // console.log(res.data);
+//     // console.log(res.data.title)
+
+    
+// }
+ 
+//   )
+  
+//   }
+
+//   const cartPost=()=> {
+//     axios
+//       .get("https://bohemian1.herokuapp.com/cartdata")
+//       .then((res) => dispatch(getCartData(res.data)))
+//       .catch((err) => console.log(err));
+//   }
+//   const handlecart=()=>{
+    
+//     axios.post('https://bohemian1.herokuapp.com/cartdata',{
+//     title:product.title,
+//     img:product.img,
+//     price:product.price,
+//     id:product.id,
+//     qty:1
+// })
+// navigate('/cart')
+//   //  window.location.href="/cart"
+   
+
+
+// }
+//  console.log(product)
+// return (
+  
+
+   
+   
+//  <div className='prdGrid'>
+        
+
+//         <div className='prdImg'>
+
+
+//  <img className='prdimgi' height='500px' width='500px' src={product.img} alt="" />
+// </div>
+// <div className='prdTit'>
+// <h3 className="prodTitle">{product.title}</h3>
+// <h5>₹{product.price}.00</h5>
+// <p className='prodP'>{product.description}</p> 
+
+// <button onClick={()=>{handlecart()}} className="cartAdd">ADD TO CART</button>
+// </div>
+
+//    </div> 
+
+
+ 
+   
+// )
+// }
 
 
